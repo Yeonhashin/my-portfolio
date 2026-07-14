64889ded14d0b02bbab89d707a4d2eddb6f26584
yeonhashin <carbeau@naver.com>
2026-07-14 18:11:37 +0900

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Portfolio from './portfolio';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Portfolio />);
